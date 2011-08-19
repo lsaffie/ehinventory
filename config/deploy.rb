a17fb4b2623d6e0ff3dcea4391d8254a1c6d9797
Luis Saffie <luis.saffie@ehealthinnovation.org>
2011-08-19 11:51:43 -0400

    getting cap to see bundled gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,11 @@
 require 'bundler/capistrano'
 
+## This get cap to see my bundled gems
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
+################
+
 set :application, "ehinventory"
 set :repository,  "git@github.com:lsaffie/ehinventory.git"
 
